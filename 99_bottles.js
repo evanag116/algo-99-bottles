138bf012b188a62a39e0b34f9901006435a09760
Evan Garcia <evanag116@yahoo.com>
2022-05-23 21:45:46 -0500

    99 bottles completed in js

--- 99_bottles.js
+++ 99_bottles.js
@@ -1,5 +1,15 @@
-function bottleSong() {
-  // Write your code here!
+function bottleSong(num) {
+  const number = num
+  for (let i = num; i > 1; i--) {
+    console.log(i + " bottles of beer on the wall, " + num + " bottles of beer");
+    num--;
+    console.log("Take one down and pass it around, " + num + " bottles of beer on the wall.")
+}
+console.log("1 bottle of beer on the wall, 1 bottle of beer.");
+console.log("Take one down and pass it around, no more bottles of beer on the wall.");
+console.log("No more bottles of beer on the wall, no more bottles of beer.");
+console.log("Go to the store and buy some more, " + number + " bottles of beer on the wall.");
+
 };
 
-bottleSong();
+bottleSong(5);
